9d91efdcb419426462fef2cfd991b69c2bc0334c
iamlegendary89-ux <iamlegendary89@gmail.com>
2025-10-06 19:14:44 +0300

    delete

--- schedule-updates.js
+++
@@ -1,5 +0,0 @@
-const cron = require('node-cron');
-const { main } = require('./update-phones');
-
-cron.schedule('0 0 * * *', () => main('daily'));
-cron.schedule('0 0 * * 0', () => main('weekly'));
