683c1cf0cb41b5fef0557619de1227975ac76ef8
iamlegendary89-ux <iamlegendary89@gmail.com>
2025-10-06 14:47:55 +0300

    Delete multiple unused files

--- script.js
+++
@@ -1,5 +0,0 @@
-fetch('http://localhost:3000/triggerUpdate').then(() => {
-    fetch('phones.json').then(r => r.json()).then(data => {
-        // Sort by data[brand].recommendationScore
-    });
-});
